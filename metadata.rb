af62e7d48c6f952951dd94ba5f43ee1d48d8b7c5
Joshua Miller <joshua.miller@fitsnips.net>
2014-03-20 10:41:00 -0700

    framwork

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'mini-dinstall'
+maintainer       'Fitsnips'
+maintainer_email 'cookbooks@fitsnips.et'
+license          'All rights reserved'
+description      'Installs/Configures mini-dinstall'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
